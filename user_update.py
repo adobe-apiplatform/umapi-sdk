8a7461e274dc2282d0eb0e63a47930714a3e035d
Daniel Brotsky <dev@brotsky.com>
2017-08-15 00:07:18 -0400

    Add a -r (--reverse) option to convert usernames to emails.
    * It's kind of hard to test this running in only one direction.  Now you can run it backwards.
    * Also added a sample users file, and only specified the columns we actually use.
    * Also simplified the logging, so we don't report successes verbosely, and we give a count.

--- user_update.py
+++ user_update.py
@@ -16,11 +16,16 @@ if __name__ == '__main__':
     parser.add_argument('-u', '--users',
                         help='filename of user file',
                         metavar='filename', dest='users_filename')
-    parser.add_argument('-t',
+    parser.add_argument('-t', '--test-mode',
                         help='run updates in test mode',
                         dest='test_mode',
                         action='store_true',
                         default=False)
+    parser.add_argument('-r', '--reverse',
+                        help='reverse conversion (go from username to email, rather than email to username)',
+                        dest='from_email',
+                        action='store_false',
+                        default=True)
 
     args = parser.parse_args()
 
@@ -31,21 +36,29 @@ if __name__ == '__main__':
                                    test_mode=args.test_mode,
                                    logger=logger)
 
-    cols = ['Identity Type', 'Username', 'Domain', 'Email', 'First Name', 'Last Name', 'Country Code',
-            'Product Configurations', 'Admin Roles', 'Product Configurations Administered']
+    cols = ['Username', 'Email']
 
     actions = {}
     for user_rec in CSVAdapter.read_csv_rows(args.users_filename, recognized_column_names=cols):
-        user = UserAction(id_type=IdentityTypes.federatedID, email=user_rec['Email'])
-        user.update(username=user_rec['Username'])
-        res = conn.execute_single(user)
-        logger.debug("Updating %s -- queued, sent, completed %s" % (user_rec['Email'], res))
-        actions[user_rec['Email']] = user
+        username, email = user_rec['Username'], user_rec['Email']
+        if args.from_email:
+            user = UserAction(id_type=IdentityTypes.federatedID, email=email)
+            user.update(username=username)
+            actions[email] = user
+        else:
+            domain = email[email.index('@') + 1:]
+            user = UserAction(id_type=IdentityTypes.federatedID, username=username, domain=domain)
+            user.update(username=email)
+            actions[username] = user
+        conn.execute_single(user)
 
     conn.execute_queued()
 
-    for email, action in actions.items():
+    successes, failures = 0, 0
+    for key, action in actions.items():
         if not action.execution_errors():
-            logger.debug("%s - updated successfully" % email)
+            successes += 1
         else:
-            logger.debug("%s - ERROR - %s" % (email, action.execution_errors()))
+            failures += 1
+            logger.error("Conversion of %s failed: %s" % (key, action.execution_errors()))
+    logger.info("Conversions attempted/succeeded/failed: %d/%d/%d" % (len(actions), successes, failures))
